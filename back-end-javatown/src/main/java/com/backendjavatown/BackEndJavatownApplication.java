7033a331490d525a12a0639fb1bb0890c58c055d
Abdessalam Benmachiche <1952175@claurendeau.qc.ca>
2025-05-28 03:23:12 -0400

    Created Springboot App

---
+++ back-end-javatown/src/main/java/com/backendjavatown/BackEndJavatownApplication.java
@@ -0,0 +1,13 @@
+package com.backendjavatown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJavatownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndJavatownApplication.class, args);
+    }
+
+}
---
+++ back-end-javatown/src/test/java/com/backendjavatown/BackEndJavatownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendjavatown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJavatownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
