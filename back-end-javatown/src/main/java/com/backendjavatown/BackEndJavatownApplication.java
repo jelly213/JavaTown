0dc6778f9099a03f50d6b9af6ea021b600192fac
Abdessalam Benmachiche <1952175@claurendeau.qc.ca>
2025-05-28 03:28:30 -0400

    Add H2 database configuration and TCP server initialization

--- back-end-javatown/src/main/java/com/backendjavatown/BackEndJavatownApplication.java
+++ back-end-javatown/src/main/java/com/backendjavatown/BackEndJavatownApplication.java
@@ -1,7 +1,10 @@
 package com.backendjavatown;
 
+import com.backendjavatown.utils.TcpServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndJavatownApplication {
@@ -10,4 +13,11 @@ public class BackEndJavatownApplication {
         SpringApplication.run(BackEndJavatownApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+            System.out.println("BackEndJavatownApplication has started successfully!");
+            TcpServer.createTcpServer("JavaTown");
+        };
+    }
 }
