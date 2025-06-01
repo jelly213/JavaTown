4e4ca5eed9cc47dec1593554a7a38ced1160ebe3
Abdessalam Benmachiche <1952175@claurendeau.qc.ca>
2025-06-01 16:44:59 -0400

    backend ready

---
+++ javatown-backend/src/main/java/com/javatownbackend/utils/TcpServer.java
@@ -0,0 +1,17 @@
+package com.javatownbackend.utils;
+
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+public class TcpServer {
+    public static void createTcpServer(String dbUrl) throws SQLException {
+        String dbPort = "9092";
+        Server tcpServer = Server.createTcpServer(
+                "-tcp", "-tcpAllowOthers", "-tcpPort", dbPort);
+        System.out.println("Tcp server start: " + tcpServer.start());
+        System.out.println(tcpServer.getStatus() + " " +
+                tcpServer.getPort());
+        System.out.println("jdbc:h2:tcp://localhost:" + dbPort + "/mem:" + dbUrl);
+    }
+}
